598686a17446e39ff12f4c81a15e63b31e803d4a
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-08-31 18:06:04 +0200

    Update 1_tt_content_tabs.php

--- Configuration/TCA/Overrides/1_tt_content_tabs.php
+++ Configuration/TCA/Overrides/1_tt_content_tabs.php
@@ -25,7 +25,7 @@ defined('TYPO3') or die('Access denied.');
                 $L.'tabsDes',
                 [
                     [
-                        ['name' => $L.'center',  'colPos' => 101, 'disallowed' => ['CType' => 'headerimage_content,2cols,3cols,4cols,image,textpic,tabs,2tabs,3tabs,4tabs,slide,accordion']]
+                        ['name' => $L.'center',  'colPos' => 101, 'disallowed' => ['CType' => 't3upheaderimage_content,2cols,3cols,4cols,image,textpic,tabs,2tabs,3tabs,4tabs,slide,accordion']]
                     ]
                 ]
                 )
