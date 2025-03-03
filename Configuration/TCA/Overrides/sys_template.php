15963f0bbc19eb5443affbf1c15db4252bb9e2c6
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-03-03 20:12:07 +0100

    Delete Configuration directory

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    't3up_container', 'Configuration/TypoScript/', 'T3UP - Container (Bootstrap)'
-);
