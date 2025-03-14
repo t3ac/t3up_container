9e0281488d0c13d36e419daa77426b7e141e175e
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-03-14 09:11:11 +0100

    14.03

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,8 @@ defined('TYPO3') || die('Access denied.');
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(static function () {
-	// Add PageTSConfig
-	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_container/Configuration/TsConfig/TCEFrames.tsconfig'");    
+    
+    // Add PageTSConfig
+	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_container/Configuration/TsConfig/TCEFrames.tsconfig'");
+    
 });
